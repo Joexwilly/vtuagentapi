fb05a4e32c8035560b9a2548ceb33c17307ff563
Joexwilly <joeguyski@gmail.com>
2023-02-07 17:27:17 +0100

    Implement get request for job

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from fastapi import Depends,HTTPException,status
+from db.repository.jobs import create_new_job,retreive_job
 
 from db.session import get_db
 from db.models.jobs import Job
@@ -14,4 +15,11 @@ router = APIRouter()
 def create_job(job: JobCreate,db: Session = Depends(get_db)):
     current_user = 1
     job = create_new_job(job=job,db=db,owner_id=current_user)
+    return job
+
+@router.get("/get/{id}",response_model=ShowJob) # if we keep just "{id}" . it would stat catching all routes
+def read_job(id:int,db:Session = Depends(get_db)):
+    job = retreive_job(id=id,db=db)
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Job with this id {id} does not exist")
     return job
