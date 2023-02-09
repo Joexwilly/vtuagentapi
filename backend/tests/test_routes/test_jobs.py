b26f89442c4c557e12a18085977bc0dfe32a140e
Joexwilly <joeguyski@gmail.com>
2023-02-09 04:34:35 +0100

    add client header to test

--- backend/tests/test_routes/test_jobs.py
+++ backend/tests/test_routes/test_jobs.py
@@ -2,19 +2,21 @@ import json
 from fastapi import status
 
 
-def test_create_job(client):
+def test_create_job(client,normal_user_token_headers):   #added normal_user_token_headers
     data = {
         "title": "SDE super",
         "company": "doogle",
         "company_url": "www.doogle.com",
         "location": "USA,NY",
         "description": "python",
-        "date_posted": "2022-03-20"
-        }
-    response = client.post("/jobs/create-job/",json.dumps(data))
-    assert response.status_code == 200 
+        "date_posted": "2022-03-20",
+    }
+    response = client.post("/jobs/create-job/",data=json.dumps(data),headers=normal_user_token_headers)  #added header in the post request
+    assert response.status_code == 200
     assert response.json()["company"] == "doogle"
     assert response.json()["description"] == "python"
+
+# We need to modify each and every unit test in which we are making a post/delete request. Since we are not restricting get requests. We do not need headers for get requests.
 
 def test_read_job(client):     #new test
     data = {
