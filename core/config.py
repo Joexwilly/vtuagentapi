e055cb7023e3bb3d3a59acb393ca1497213adaba
Joexwilly <joeguyski@gmail.com>
2023-03-21 09:31:36 +0100

    make it better

--- core/config.py
+++ core/config.py
@@ -18,8 +18,8 @@ class Settings(BaseSettings):
     POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER")
     POSTGRES_PORT : str = os.getenv(" POSTGRES_PORT","5432") # default postgres port is 5432
     POSTGRES_DB : str = os.getenv("POSTGRES_DB")
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    
+   # DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    DATABASE_URL = os.getenv("DATABASE_URLSQL")
     SECRET_KEY :str = os.getenv("SECRET_KEY")   
     ALGORITHM = "HS256"                         
     ACCESS_TOKEN_EXPIRE_MINUTES = 30  
